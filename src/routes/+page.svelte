61c9ff39184d9d0fa290c83cbb176e9adf3760f0
KosumovicToni <kosumovic.toni@gmail.com>
2026-02-02 15:46:05 +0100

    formatted file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,21 +7,28 @@
 </script>
 
 <div class="flex flex-col w-screen h-screen">
-  <div class="flex flex-col h-screen w-screen items-center justify-center bg-white">
+  <div
+    class="flex flex-col h-screen w-screen items-center justify-center bg-white"
+  >
     <div class="grid grid-cols-1 lg:grid-cols-4 w-screen justify-items-center">
-      <div class="grd grd-cols-1 justify-item-center text-center font-bold lg:ml-auto my-5 border-b-2 lg:border-none">
+      <div
+        class="grd grd-cols-1 justify-item-center text-center font-bold lg:ml-auto my-5 border-b-2 lg:border-none"
+      >
         <h1 class="text-3xl mb-2">Lista Farmaci</h1>
-        <button class="font-bold rounded-lg border-2 w-20 transition duration-500 opacity-60 hover:opacity-100">+</button>
+        <button
+          class="font-bold rounded-lg border-2 w-20 transition duration-500 opacity-60 hover:opacity-100"
+          >+</button
+        >
       </div>
       <div class="col-span-3">
         {#each { length: rows + 1 } as _, i}
-          <div 
-            class="grid justify-items-center gap-4 py-1" 
+          <div
+            class="grid justify-items-center gap-4 py-1"
             style="grid-template-columns: repeat({cols + 1}, minmax(0, 40px));"
           >
             {#if i === 0}
-              <div class="w-10"></div> 
-              
+              <div class="w-10"></div>
+
               {#each { length: cols } as _, j}
                 <div class="flex items-center justify-center w-12 h-12">
                   <p class="font-bold text-black text-sm">{j + 1}</p>
@@ -31,13 +38,13 @@
               <div class="flex items-center justify-center w-12 h-12">
                 <p class="font-bold text-black text-sm">{alf[i]}</p>
               </div>
-      
+
               {#each { length: cols } as _, j}
-                  <Pot />
+                <Pot />
               {/each}
             {/if}
           </div>
-        {/each} 
+        {/each}
       </div>
     </div>
   </div>
@@ -45,11 +52,11 @@
     <div class="grid grid-cols-2">
       <div>
         <label for="">Rows : </label>
-        <input type="number" min="1" max ="12" bind:value={rows}>
+        <input type="number" min="1" max="12" bind:value={rows} />
       </div>
       <div>
         <label for="">Columns : </label>
-        <input type="number" min="1" max ="12" bind:value={cols}>
+        <input type="number" min="1" max="12" bind:value={cols} />
       </div>
     </div>
   </div>
