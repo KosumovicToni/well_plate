5e6f22ed505f4b47450d810542960ee6ebfa3ad4
KosumovicToni <kosumovic.toni@gmail.com>
2026-02-05 18:51:50 +0100

    resposive design added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,60 +23,58 @@
 
 <div class="flex flex-col w-screen h-screen">
   <Navbar />
-  <div class="flex h-screen w-screen items-center bg-white">
-    <div class="grid grid-cols-1 lg:grid-cols-4 w-screen justify-items-center">
-      <div
-        class="flex flex-col items-center text-center font-bold lg:ml-auto my-5"
-      >
-        <h1 class="text-3xl mb-2">Lista Farmaci</h1>
+  <div
+    class="flex flex-col md:flex-row lg:flex-row h-screen w-screen items-center bg-white"
+  >
+    <div class="flex flex-col grow text-center font-bold lg:ml-auto my-5">
+      <h1 class="text-3xl mb-2">Lista Farmaci</h1>
 
-        {#each farmaci as farmaco}
-          <Farmaco
-            name={farmaco.name}
-            dose={farmaco.dose}
-            unit={farmaco.unit}
-            color={farmaco.color}
-          />
-        {/each}
-        <div class="relative">
-          <button
-            class="font-bold rounded-lg border-2 w-20 transition duration-500 opacity-60 hover:opacity-100"
-            onclick={() => {
-              pop = !pop;
-            }}>+</button
-          >
+      {#each farmaci as farmaco}
+        <Farmaco
+          name={farmaco.name}
+          dose={farmaco.dose}
+          unit={farmaco.unit}
+          color={farmaco.color}
+        />
+      {/each}
+      <div class="relative">
+        <button
+          class="font-bold rounded-lg border-2 w-20 transition duration-500 opacity-60 hover:opacity-100"
+          onclick={() => {
+            pop = !pop;
+          }}>+</button
+        >
 
-          {#if pop}
-            <AddFarmaco bind:pop bind:farmaci />
+        {#if pop}
+          <AddFarmaco bind:pop bind:farmaci />
+        {/if}
+      </div>
+    </div>
+    <div class="flex flex-col grow">
+      {#each { length: rows + 1 } as _, i}
+        <div
+          class="grid justify-items-center gap-4 py-1"
+          style="grid-template-columns: repeat({cols + 1}, minmax(0, 40px));"
+        >
+          {#if i === 0}
+            <div class="w-10"></div>
+
+            {#each { length: cols } as _, j}
+              <div class="flex items-center justify-center w-12 h-12">
+                <p class="font-bold text-black text-sm">{j + 1}</p>
+              </div>
+            {/each}
+          {:else}
+            <div class="flex items-center justify-center w-12 h-12">
+              <p class="font-bold text-black text-sm">{alf[i]}</p>
+            </div>
+
+            {#each { length: cols } as _}
+              <Pot {farmaci} />
+            {/each}
           {/if}
         </div>
-      </div>
-      <div class="col-span-3">
-        {#each { length: rows + 1 } as _, i}
-          <div
-            class="grid justify-items-center gap-4 py-1"
-            style="grid-template-columns: repeat({cols + 1}, minmax(0, 40px));"
-          >
-            {#if i === 0}
-              <div class="w-10"></div>
-
-              {#each { length: cols } as _, j}
-                <div class="flex items-center justify-center w-12 h-12">
-                  <p class="font-bold text-black text-sm">{j + 1}</p>
-                </div>
-              {/each}
-            {:else}
-              <div class="flex items-center justify-center w-12 h-12">
-                <p class="font-bold text-black text-sm">{alf[i]}</p>
-              </div>
-
-              {#each { length: cols } as _}
-                <Pot {farmaci} />
-              {/each}
-            {/if}
-          </div>
-        {/each}
-      </div>
+      {/each}
     </div>
   </div>
   <Footer bind:rows bind:cols />
